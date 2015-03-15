3109aeaab6cd7067b867b6a6a454de1f08aec856
Paweł Szulc <paul.szulc@gmail.com>
2015-03-15 23:47:45 +0100

    comments added to example 1

--- src/main/scala/ex1/StreamingHelloWorldOverSocket.scala
+++ src/main/scala/ex1/StreamingHelloWorldOverSocket.scala
@@ -3,13 +3,17 @@ package ex1
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+/**
+ * Run nc -lk 9999 to create a socket server
+ * Type words into console and see them printed out back in our driver program
+ */
 object StreamingHelloWorldOverSocket extends App {
 
   val conf = new SparkConf()
     .setMaster("local[*]")
     .setAppName("Simple Streaming")
 
-  val ssc = new StreamingContext(conf, Seconds(1))
+  val ssc = new StreamingContext(conf, Seconds(10))
   val lines = ssc.socketTextStream("localhost", 9999)
   val lowered = lines.filter(!_.isEmpty).map(_.toLowerCase)
   lowered.print()
